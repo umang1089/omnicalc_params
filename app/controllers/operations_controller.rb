f55199507f9f95977356e64868974b3251693330
Umang Shukla <Umang1089@gmail.com>
2017-04-28 23:04:53 -0500

    Basic functionality works
    Need to investigate rounding on payment, but ready to move on to forms

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -15,4 +15,24 @@ class OperationsController < ApplicationController
     render("operations/square_root.html.erb")
   end
 
+  def payment
+    @basis_points = params["basis_points"].to_f
+    @r = @basis_points.to_f / 10000 / 12 # convers basis points to monthly APR as decimal
+    @years = params["number_of_years"].to_f
+    @n = @years * 12 # convert years to number of months
+    @pv = params["present_value"].to_f
+
+    @output = (@r*@pv*(1+@r)**@n)/((1+@r)**@n - 1)
+
+    render("operations/payment.html.erb")
+  end
+
+  def random
+    @minimum = params["minimum"].to_i
+    @maximum = params["maximum"].to_i
+
+    @output = rand(@minimum...@maximum)
+
+    render("operations/random.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get("/flexible/square/:number", { :controller => "operations", :action => "square"})
 
   get("/flexible/square_root/:number", { :controller => "operations", :action => "square_root"})
+
+  get("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller => "operations", :action => "payment"})
+
+  get("/flexible/random/:minimum/:maximum", { :controller => "operations", :action => "random" })
 end
