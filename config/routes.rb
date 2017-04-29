c2cda76a32f2cd58bc90cb4c56042e8409be7270
Umang Shukla <Umang1089@gmail.com>
2017-04-28 22:32:00 -0500

    re-updated routes file
    need to figure out how branches work... and why it defaulted back to
    master.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get("/flexible/square/:number", { :controller => "operations", :action => "square"})
+
+  get("/flexible/square_root/:number", { :controller => "operations", :action => "square_root"})
 end
