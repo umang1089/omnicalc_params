ff5cb1bdd3b01695cd6ccfecdfcdbaa7a74d8bf4
Umang Shukla <Umang1089@gmail.com>
2017-04-28 22:25:15 -0500

    Revert "Squares and Squareroots Functional"
    This reverts commit f7871d4974d268f4ec66268edbf86b0ba8ba6e93.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Flexible Square
-  get("/flexible/square/:number", { :controller => "operations", :action => "square"})
-
-  #Flexible Squareroot
-  get("/flexible/square_root/:number", { :controller => "operations", :action => "square_root"})
 end
